3c039528731d8dd0ef8318ec4b152761bd3b6614
Nvlado <163466421+Nvlado@users.noreply.github.com>
2024-03-24 08:17:48 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+
+const [inputText, setInputText] = useState("")
+
+function handleChange(event){
+  const newValue = event.target.value
+  setInputText(newValue)
+}
+
+function App() {
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText}/>
+        <button>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          <li>A Item</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
